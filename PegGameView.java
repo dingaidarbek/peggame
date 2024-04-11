fe6faaff8e452f121691d9dd0b7aa913900dd334
Lana <lana.mohd006@gmail.com>
2024-04-11 14:25:28 +0400

    add comments

--- PegGameView.java
+++ PegGameView.java
@@ -12,9 +12,19 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+/*
+ * This class provide the GUI implementation of the Peg Game
+ */
 
 public class PegGameView extends Application {
 
+    /**
+     * The method makeLabel(String text, int size, Color foreground) is a factory method that is used to create 
+     * labels and customize them.
+     * @param text this specifies the text you want to be written inside the label.
+     * @param size specifies the size of the text.
+     * @param foreground color of the text.
+     */
     private static Label makeLabel(String text, int size, Color foreground){
         Label myLabel = new Label(text);
         myLabel.setFont(new Font("Times New Roman", size));
@@ -29,6 +39,7 @@ public class PegGameView extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("The Peg Game");
 
+        // Constructing the controls for the peg game.
         VBox vbox= new VBox();
         vbox.setAlignment(Pos.CENTER);
         Label message = makeLabel("Welcome To The Peg Game!", 36,  Color.BLACK);
@@ -36,23 +47,23 @@ public class PegGameView extends Application {
         String gs = (String)(getGameState());
         TextField path = new TextField();
         Label state = makeLabel("current state of the game: " + gs, 24, Color.BLACK);
-        //vbox.getChildren().addAll(message, prompt, path);
+        
 
-
+        // reading th eboard from the text file.
         ReadFile read = new ReadFile();
         String boardPath = path.getText();
         PegGameSquare game = read.readFile(boardPath);
         int length = game.getLength();
-        char[][] b = game.getGameBoard();
+        char[][] board = game.getGameBoard();
         
     
-        // Creating the board
+        // Creating the board using gridpane layout
         GridPane gridPane = new GridPane();
         gridPane.setPadding(new Insets(length+1));
         gridPane.setAlignment(Pos.CENTER);
         for(int col=1; col<=length; col++) {
             for(int row=1; row<=length; row++) {  
-                char value = b[row][col];  
+                char value = board[row][col];  
                 if(value == '-'){
                     Label empty = makeLabel("-", 24,  Color.RED);
                     empty.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
@@ -64,10 +75,13 @@ public class PegGameView extends Application {
                     gridPane.add(peg, col, row);
                 }
            
-         }
- }
+            }
+        }
 
-    vbox.getChildren().addAll(message, prompt, path, state, gridPane);
+        // puting all the controls into the vertical box.
+        vbox.getChildren().addAll(message, prompt, path, state, gridPane);
+
+        // adding the vbox into a scene
         Scene scene = new Scene(vbox, 300, 500);
 
         primaryStage.setScene(scene);
